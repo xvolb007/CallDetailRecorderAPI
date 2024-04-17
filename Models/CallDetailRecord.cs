ca0a00b3d18e5683f421704de272eff24abbe6d1
xvolb007 <unicartoon2002@gmail.com>
2024-04-17 20:22:06 +0200

    CallDetailRecord model updated and dependencies for dataaccess lib

--- Models/CallDetailRecord.cs
+++ Models/CallDetailRecord.cs
@@ -2,6 +2,14 @@
 {
     public class CallDetailRecord
     {
-
+        public int? Id { get; set; }
+        public int CallerId { get; set; }
+        public int Recipient { get; set; }
+        public DateOnly CallDate { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public int Duration { get; set; }
+        public decimal Cost { get; set; }
+        public string Reference { get; set; }
+        public string Currency { get; set; }
     }
 }
