548216ce33043854699bf481f966f99a9144857a
xvolb007 <unicartoon2002@gmail.com>
2024-04-17 20:32:38 +0200

    database configuration and repository injection

--- CallDetailRecorderAPI/Program.cs
+++ CallDetailRecorderAPI/Program.cs
@@ -1,3 +1,7 @@
+using DataAccess;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<ICallDetailRecordRepository, CallDetailRecordRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
